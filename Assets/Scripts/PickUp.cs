c39a4d3db8725c4b09026dfdf658359edca66ef7
Austin <austinessinger@gmail.com>
2019-09-20 16:13:15 -0400

    added FIRE

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform target;
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        this.transform.position = target.position;
+        this.transform.parent = GameObject.Find("FPSController").transform;
+        this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = GameObject.Find("FPSController").transform;
+        this.transform.parent = null;
+    }
+}
